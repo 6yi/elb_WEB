30bd5627f8edbaa7b84459701b8058f531034c65
6yi <lzhengycy@outlook.com>
2019-07-16 19:10:48 +0800

    new

---
+++ src/main/java/com/lzheng/elbweb/controller/workCon.java
@@ -0,0 +1,98 @@
+package com.lzheng.elbweb.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.lzheng.elbweb.entities.Msg;
+import com.lzheng.elbweb.service.UserService;
+import com.lzheng.elbweb.service.workService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName workCon
+ * @Author lzheng
+ * @Date 2019/6/28 13:06
+ * @Version 1.0
+ * @Description:
+ */
+
+@Controller
+public class workCon {
+    @Autowired
+    private workService service;
+
+    @Autowired
+    private UserService uservice;
+
+
+    //查询水电==========================================================================================================
+
+    @GetMapping("/work/query")
+    public void cx(@RequestParam("loudong")String loudong,@RequestParam("sushe")String sushe ,HttpServletRequest request,HttpServletResponse response) throws IOException {
+
+        System.out.println("ajax有反应了！！"+loudong+"xx"+sushe);
+
+        String tokne=(String)request.getSession().getAttribute("token");
+        String userid=(String)request.getSession().getAttribute("userid");
+
+        List<String> list=new ArrayList<>();
+
+        if (tokne==null){
+             list=uservice.query("lzheng","13650010553");
+            tokne=list.get(0);
+            userid=list.get(1);
+        }
+
+
+        String bulid=service.query(loudong,sushe,tokne,userid);
+        request.getSession().setAttribute("msg",bulid);
+        PrintWriter out = response.getWriter();
+        out.print(bulid);
+
+//        return bulid;
+
+    }
+
+
+    @GetMapping("/work/queryResult")
+    public String queryResult(HttpServletRequest request,HttpServletResponse response){
+       String result[]= service.queryResult((String) request.getSession().getAttribute("token"),
+                (String) request.getSession().getAttribute("userid"));
+       String results[][]=new String[result.length-1][4];
+
+       //这里是切割获取的字符串
+       for (int i=1;i<result.length;i++){
+            int z=1;
+           for(String strs:result[i].split(",")){
+                   results[i-1][z]=strs;
+                   z++;
+            }
+
+            String obj[]=results[i-1][1].split(":");
+            results[i-1][0]=obj[0];
+            results[i-1][1]=obj[1];
+       }
+
+//       result[0]="亲爱的"+request.getSession().getAttribute("username")+"以下是你本学期的成绩噢！";
+
+       request.getSession().setAttribute("result",results);
+       request.getSession().setAttribute("hl","亲爱的"+request.getSession().getAttribute("username")+"以下是你本学期的成绩噢！");
+       request.getSession().setAttribute("l","jw");
+       return "result";
+
+    }
+
+
+
+}
---
+++ src/main/java/com/lzheng/elbweb/service/UserService.java
@@ -0,0 +1,128 @@
+package com.lzheng.elbweb.service;
+
+import com.lzheng.elbweb.entities.MD5;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @ClassName UserService
+ * @Author lzheng
+ * @Date 2019/6/28 10:30
+ * @Version 1.0
+ * @Description:
+ */
+
+@Service
+public class UserService {
+    private static Pattern pattern = Pattern.compile("\"xm\":.{4,5}");
+
+    //这个query是 queryToken 的意思，，，当初写的时候没注意。。命名不规范，找BUG两行泪
+
+    public List<String> query(String password, String username){
+        String val="";
+        String userid="";
+        String cookie="";
+        List<String> parms=new ArrayList<>();
+
+        //默认使用我的ID和账号去查询水电
+        String passwordCont;
+        String SignCont = "sign=2";
+        String userNameCont;
+
+        if(!username.startsWith("2")){
+            SignCont="sign=3";
+        }
+
+        try {
+            URL url=new URL("https://smart.ccdgut.edu.cn/app/user/login.do");
+            HttpURLConnection con= (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("POST");
+            con.setUseCaches(true);
+
+            //伪造请求头redirect
+            con.setRequestProperty("User-Agent", "MI 6/1.8(Android;8.0.0;1080x1790;s;46000;wifi)");
+            con.setRequestProperty("DevicesId", "MI 6/866654034887460");
+            con.setRequestProperty("XPS-PUSHID", "AtDGuURXRLxp4Lo7cwFwbOfS___dBGmcoQG0oaiWPFK3");
+            con.setRequestProperty("XPS-ClientCode", "csxy");
+            con.setRequestProperty("schoolCode", "csxy");
+            con.setRequestProperty("Content-Type", " application/x-www-form-urlencoded");
+            con.setRequestProperty("Content-Length", "68");
+            con.setRequestProperty("Host", "smart.ccdgut.edu.cn");
+            con.setRequestProperty("Connection", "Keep-Alive");
+            con.setRequestProperty("Accept-Encoding", "gzip");
+            //请求参数
+            con.setDoOutput(true);
+            con.setDoInput(true);
+            con.connect();
+            DataOutputStream out = new DataOutputStream(con
+                    .getOutputStream());
+
+
+
+            passwordCont = "password="+ MD5.getStringMD5String(MD5.getStringMD5String("jw134#%pqNLVfn"+password));
+            System.out.println("psw==="+passwordCont);
+            userNameCont="account="+username;
+
+
+            out.writeBytes(passwordCont+"&"+SignCont+"&"+userNameCont);
+            //流用完记得关
+            out.flush();
+            out.close();
+            Map<String, List<String>> headers = con.getHeaderFields();
+            Set<String> keys = headers.keySet();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+            String name="";
+            String line;
+            line = reader.readLine();
+
+
+            Matcher matcher = pattern.matcher(line);
+            String result="";
+            //查找符合规则的子串
+            while(matcher.find()){
+                //获取字符串
+                result=matcher.group();
+
+            }
+
+
+            if (!result.isEmpty()){
+                name=result.split(":")[1].replaceAll("\"","");
+            }
+
+
+            val = con.getHeaderField("Xps-Usertoken");
+            userid=con.getHeaderField("Xps-UserId");
+            cookie=con.getHeaderField("Set-Cookie");
+
+            parms.add(val);
+            parms.add(userid);
+            parms.add(cookie);
+            parms.add(name);
+
+            reader.close();
+            con.disconnect();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return parms;
+
+    }
+
+
+
+}
