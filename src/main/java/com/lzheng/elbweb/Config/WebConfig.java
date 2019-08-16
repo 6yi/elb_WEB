ce4c9a97d69faf806efacc4c648cc7b2ecbb5065
6yi <lzhengycy@outlook.com>
2019-08-16 18:25:50 +0800

    x

--- src/main/java/com/lzheng/elbweb/Config/WebConfig.java
+++ src/main/java/com/lzheng/elbweb/Config/WebConfig.java
@@ -23,12 +23,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-
         // addPathPatterns("/**") 表示拦截所有的请求，
         // excludePathPatterns("/login", "/register") 表示除了登陆与注册之外，因为登陆注册不需要登陆也可以访问
 
         //妈的，我使用拦截全部，然后排除部分的时候有问题！！！只能曲线救国了
-
 
         registry.addInterceptor(loginInterceptor).addPathPatterns("/user/recharge","/work/queryResult","/work/queryClass");
 
